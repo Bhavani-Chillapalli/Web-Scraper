42d0b43139c5cf8f366d10c041a828daf13765a4
bhavani  chillapalli <chillapallibhavani@gmail.com>
2017-02-15 18:20:41 -0500

    Initial Commit.

---
+++ scraping_links.py
@@ -0,0 +1,41 @@
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+from time import sleep
+
+BASE_URL = "http://www.chicagoreader.com"
+
+def get_category_links(section_url):
+	html = urlopen(section_url).read()
+	soup = BeautifulSoup(html, "lxml")
+	boccat = soup.find("dl", "boccat")
+	category_links = [BASE_URL + dd.a["href"] for dd in boccat.findAll("dd")]
+	return category_links
+
+def get_category_winner(category_url):
+	html = urlopen(category_url).read()
+	soup = BeautifulSoup(html, "lxml")
+	category = soup.find("h1", "headline").string
+	winner = [h2.string for h2 in soup.findAll("h2", "boc1")]
+	runners_up = [h2.string for h2 in soup.findAll("h2", "boc2")]
+	return {"category": category,
+			"category_url": category_url,
+			"winner": winner,
+			"runners_up": runners_up}
+
+def make_soup(url):
+	html = urlopen(url).read()
+	return BeautifulSoup(html, "lxml")
+
+if __name__ == '__main__':
+	food_n_drink = ("http://www.chicagoreader.com/chicago/"
+                    "best-of-chicago-2011-food-drink/BestOf?oid=4106228")
+
+	categories = get_category_links(food_n_drink)
+
+	data = []
+	for category in categories:
+		winner = get_category_winner(category)
+		data.append(winner)
+		sleep(1)
+
+	print data
